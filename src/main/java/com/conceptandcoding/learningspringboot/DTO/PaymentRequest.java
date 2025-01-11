ff780f48f0fc1f64d45355d55b3b115c6b226050
guddu <ksguddu262@gmail.com>
2025-01-11 11:57:00 +0530

    devloped default layer architecture of spring boot

---
+++ src/main/java/com/conceptandcoding/learningspringboot/DTO/PaymentRequest.java
@@ -0,0 +1,12 @@
+package com.conceptandcoding.learningspringboot.DTO;
+
+public class PaymentRequest {
+    private long paymentId;
+
+    public void setPaymentId(long paymentId) {
+        this.paymentId = paymentId;
+    }
+    public long getPaymentId() {
+        return paymentId;
+    }
+}
